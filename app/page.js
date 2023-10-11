9abf800445d7c514c7ceb834c4ca94bb5830b3bd
Joshua Lee <85899358+Joshua-S-Lee@users.noreply.github.com>
2023-10-10 23:45:17 -0500

    setup complete

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-// This is home, the default page. When going to the base domain this is what will be rendered.
-export default function Home() {
-  return (
-    <main className='bg-green-500'>
-      <h1>Hello World</h1>
-    </main>
-  )
-}
