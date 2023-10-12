3b02377559e43abc0efaf9c71da55386ede6db0f
Joshua Lee <85899358+Joshua-S-Lee@users.noreply.github.com>
2023-10-11 22:11:30 -0500

    home basics

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
 
 const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is a thing that does stuff. It's pretty cool. something blah blah blah about ai and prompts and stuff.
+      </p>
+      {/* list component */}
+
+    </section>
   )
 }
 
